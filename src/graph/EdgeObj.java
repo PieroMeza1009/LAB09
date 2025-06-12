388b22ff286ffd30a4a05bf884c2a6194ca1cf6f
PieroMeza10 <pieroms0905@gmail.com>
2025-06-12 15:24:14 -0500

    3ejer6

--- src/graph/EdgeObj.java
+++ src/graph/EdgeObj.java
@@ -5,7 +5,7 @@ public class EdgeObj<V, E> {
     protected VertexObj<V, E> endVertex1;
     protected VertexObj<V, E> endVertex2;
     protected int position;
-    
+
     public EdgeObj(VertexObj<V, E> vert1, VertexObj<V, E> vert2, E info, int position) {
         this.endVertex1 = vert1;
         this.endVertex2 = vert2;
@@ -19,5 +19,19 @@ public class EdgeObj<V, E> {
 
     public VertexObj<V, E> getEndVertex2() {
         return endVertex2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof EdgeObj<?, ?> e) {
+            return (endVertex1.equals(e.endVertex1) && endVertex2.equals(e.endVertex2)) ||
+                   (endVertex1.equals(e.endVertex2) && endVertex2.equals(e.endVertex1));
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + endVertex1 + " - " + endVertex2 + ")";
     }
 }
