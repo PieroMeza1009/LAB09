b6ad744394f058ea89ecbbce44f0912f4f09b979
PieroMeza10 <pieroms0905@gmail.com>
2025-06-12 15:22:28 -0500

    3ejer1

--- src/graph/VertexObj.java
+++ src/graph/VertexObj.java
@@ -1,5 +1,11 @@
 package graph;
 
-public class VertexObj {
+public class VertexObj<V, E> {
+    protected V info;
+    protected int position;
 
+    public VertexObj(V info, int position) {
+        this.info = info;
+        this.position = position;
+    }
 }
