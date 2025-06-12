55fb3d73b152434f9af546702ffb7dc1e16527c4
PieroMeza10 <pieroms0905@gmail.com>
2025-06-12 15:24:57 -0500

    3ejer22

--- src/graph/GraphListEdge.java
+++ src/graph/GraphListEdge.java
@@ -9,4 +9,15 @@ public class GraphListEdge<V, E> {
         this.secVertex = new ArrayList<>();
         this.secEdge = new ArrayList<>();
     }
+
+
+    public void insertVertex(V data) {
+        if (!searchVertex(data)) {
+            secVertex.add(new VertexObj<>(data, secVertex.size()));
+        }
+    }
+
+    public void insertEdge(V v, V z) {
+        VertexObj<V, E> v1 = getVertex(v);
+        VertexObj<V, E> v2 = getVertex(z);
 }
